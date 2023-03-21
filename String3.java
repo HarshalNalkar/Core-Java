a849107b651205c84f35fa9d9f5e80ad2ba2be07
Harshal Nalkar <95092091+HarshalNalkar@users.noreply.github.com>
2023-03-21 11:09:12 +0530

    Update String3.java

--- String3.java
+++ String3.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 class String3
 {
+	//Write a program to check whether the given string of index.
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
