09677edb9a9898a5813fda587bb0c5acf0c36810
Harshal Nalkar <95092091+HarshalNalkar@users.noreply.github.com>
2023-03-21 10:46:24 +0530

    Write a program to check whether the given string of index.

---
+++ String3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class String3
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter the string");
+		String s = sc.next();
+
+		System.out.println("enter any index value");
+		int i = sc.nextInt();
+
+		System.out.println(s.length());
+		System.out.println(s.charAt(i));
+	}
+}
